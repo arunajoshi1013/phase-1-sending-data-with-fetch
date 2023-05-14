aa955de09021ecdddcd0a99d30c602bc9fc4e781
Aruna Joshi <arunajoshi1013@gmail.com>
2023-05-13 17:33:59 -0700

    fetch data

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+
+function submitData(name, email) {
+  const url = 'http://localhost:3000/users';
+  const data = { name: name, email: email };
+
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+    const id = data.id;
+    document.body.append(id);
+  })
+  .catch(error => {
+    console.log(error.message);
+    document.body.append(error.message);
+  });
+}
